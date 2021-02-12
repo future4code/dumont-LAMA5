9254909879a044dc1c0e4c7b4e0c0a156d0f07ab
Vanessasz <vanessa182vans@gmail.com>
2021-02-12 10:21:40 -0300

    Primeiro endpoint ok

---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,54 @@
+import { BaseDatabase } from "./BaseDatabase";
+import { User } from "../business/entities/User";
+import { CustomError } from "../business/error/CustomError";
+
+export class UserDatabase extends BaseDatabase {
+
+   private static TABLE_NAME = "USERS"
+
+   private static toUserModel(user: any): User {
+      return new User(
+         user.id,
+         user.name,
+         user.email,
+         user.password,
+         User.stringToUserRole(user.role)
+      );
+   }
+
+   public async createUser(
+      id: string,
+      name: string,
+      email: string,
+      password: string,
+      role: string
+   ): Promise<void> {
+      try {
+         await BaseDatabase.connection
+            .insert({
+               id,
+               name,
+               email,
+               password,
+               role
+            })
+            .into(UserDatabase.TABLE_NAME);
+            
+      } catch (error) {
+         console.log(error)
+         throw new CustomError(500, "An unexpected error ocurred");
+      }
+   }
+
+   public async getUserByEmail(email: string): Promise<User> {
+      try {
+         const result = await BaseDatabase.connection
+            .select("*")
+            .from(UserDatabase.TABLE_NAME)
+            .where({ email });
+         return UserDatabase.toUserModel(result[0]);
+      } catch (error) {
+         throw new CustomError(500, "An unexpected error ocurred");
+      }
+   }
+}
